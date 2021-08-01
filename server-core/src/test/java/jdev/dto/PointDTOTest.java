d9f001e553e231f4f90933d48ca01904b0594a62
zhurindv <zhurindv@gmail.com>
2021-08-01 17:31:56 +0700

    копирование PointDTOTest из common во все модули

---
+++ server-core/src/test/java/jdev/dto/PointDTOTest.java
@@ -0,0 +1,37 @@
+package jdev.dto;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Дмитрий on 31.07.2021.
+ */
+public class PointDTOTest {
+
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1627800494452}";
+    private String autoId = "o567gfd";
+
+    @Test
+    public void toJson() throws Exception {
+        PointDTO point = new PointDTO();
+        point.setLat(56);
+        point.setLon(74);
+        point.setAutoId("o567gfd");
+        point.setTime(System.currentTimeMillis());
+        assertTrue(point.toJson().contains("\"lat\":56"));
+        assertTrue(point.toJson().contains("\"time\":"));
+        System.out.println(point.toJson());
+    }
+
+    @Test
+    public void decodeDto() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        PointDTO dto = mapper.readValue(expected, PointDTO.class);
+        assertEquals(autoId, dto.getAutoId());
+        assertEquals(1627800494452L, dto.getTime());
+    }
+}
