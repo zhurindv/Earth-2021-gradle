b5a181c3a186475bfc74b584ce43e4149c64f0aa
zhurindv <zhurindv@gmail.com>
2021-08-01 22:11:18 +0700

    добавление класса Main во все модули(кроме common)

---
+++ server-ui/src/main/java/jdev/server_ui/Main.java
@@ -0,0 +1,18 @@
+package jdev.server_ui;
+
+import jdev.dto.PointDTO;
+
+/**
+ * Created by Дмитрий on 01.08.2021.
+ */
+public class Main {
+    public static void main(String... args) throws Exception {
+        for (int i=0; i<5; i++) {
+            System.out.println("Main.main say Hello!!!!");
+            PointDTO point = new PointDTO();
+            point.setLat(45);
+            System.out.println(point.toJson());
+            Thread.sleep(1000);
+        }
+    }
+}
